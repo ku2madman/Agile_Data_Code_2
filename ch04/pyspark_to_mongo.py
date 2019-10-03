b81f311c3654ef2303df30101d6df6b353781c0f
昆 <ku_madman@hotmail.com>
2019-10-03 13:34:23 +0800

    use mongo-spark-connector instead mongo-hadoop

--- ch04/pyspark_to_mongo.py
+++ ch04/pyspark_to_mongo.py
@@ -1,10 +1,31 @@
-import pymongo
-import pymongo_spark
-# Important: activate pymongo_spark.
-pymongo_spark.activate()
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.master('local') \
+    .config("spark.mongodb.input.uri", "mongodb://127.0.0.1/agile_data_science.on_time_performance") \
+    .config("spark.mongodb.output.uri", "mongodb://127.0.0.1/agile_data_science.on_time_performance") \
+    .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.11:2.4.1") \
+    .getOrCreate()
 
 on_time_dataframe = spark.read.parquet('data/on_time_performance.parquet')
 
-# Note we have to convert the row to a dict to avoid https://jira.mongodb.org/browse/HADOOP-276
-as_dict = on_time_dataframe.rdd.map(lambda row: row.asDict())
-as_dict.saveToMongoDB('mongodb://localhost:27017/agile_data_science.on_time_performance')
+# convert "float" to "double", to avoid "FloatType has no matching BsonValue" exception
+on_time_dataframe = on_time_dataframe.withColumn("DepDelay", on_time_dataframe["DepDelay"].cast('double'))
+on_time_dataframe = on_time_dataframe.withColumn("TaxiOut", on_time_dataframe["TaxiOut"].cast('double'))
+on_time_dataframe = on_time_dataframe.withColumn("ArrDelay", on_time_dataframe["ArrDelay"].cast('double'))
+on_time_dataframe = on_time_dataframe.withColumn("TaxiIn", on_time_dataframe["TaxiIn"].cast('double'))
+on_time_dataframe = on_time_dataframe.withColumn("ArrDelay", on_time_dataframe["ArrDelay"].cast('double'))
+on_time_dataframe = on_time_dataframe.withColumn("ArrDelayMinutes", on_time_dataframe["ArrDelayMinutes"].cast('double'))
+on_time_dataframe = on_time_dataframe.withColumn("ActualElapsedTime", on_time_dataframe["ActualElapsedTime"].cast('double'))
+on_time_dataframe = on_time_dataframe.withColumn("AirTime", on_time_dataframe["AirTime"].cast('double'))
+on_time_dataframe = on_time_dataframe.withColumn("Distance", on_time_dataframe["Distance"].cast('double'))
+on_time_dataframe = on_time_dataframe.withColumn("CarrierDelay", on_time_dataframe["CarrierDelay"].cast('double'))
+on_time_dataframe = on_time_dataframe.withColumn("WeatherDelay", on_time_dataframe["WeatherDelay"].cast('double'))
+on_time_dataframe = on_time_dataframe.withColumn("NASDelay", on_time_dataframe["NASDelay"].cast('double'))
+on_time_dataframe = on_time_dataframe.withColumn("SecurityDelay", on_time_dataframe["SecurityDelay"].cast('double'))
+on_time_dataframe = on_time_dataframe.withColumn("LateAircraftDelay", on_time_dataframe["LateAircraftDelay"].cast('double'))
+# print(on_time_dataframe.dtypes)
+
+on_time_dataframe.write.format('mongo').mode('overwrite').save()
+
+df = spark.read.format('mongo').load()
+df.show()
