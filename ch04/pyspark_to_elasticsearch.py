fc4b5849d9cb5a1accd1d7356f090ef7c8e26482
昆 <ku_madman@hotmail.com>
2019-10-11 18:53:22 +0800

    set elasticsearch url obviously

--- ch04/pyspark_to_elasticsearch.py
+++ ch04/pyspark_to_elasticsearch.py
@@ -3,6 +3,7 @@ on_time_dataframe = spark.read.parquet('data/on_time_performance.parquet')
 
 # Save the DataFrame to Elasticsearch
 on_time_dataframe.write.format("org.elasticsearch.spark.sql")\
+  .option("es.nodes", "localhost")
   .option("es.resource","agile_data_science/on_time_performance")\
   .option("es.batch.size.entries","100")\
   .mode("overwrite")\
